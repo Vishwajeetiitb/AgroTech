8b814b89077aea3b016d5aab7cf26dff8ba93db2
vishwajeetiitb <vishwajeet724728@gmail.com>
2020-10-28 10:08:43 +0530

    gimbal issue resolved

--- test.py
+++ test.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
-import roslib
-import sys
 import rospy
-import cv2
-from std_msgs.msg import String
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-bridge = CvBridge()
-def callback(msg):
-	cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
-	cv_image = cv2.resize(cv_image, (470, 250))
-	cv2.imshow("Image window", cv_image)
-	cv2.waitKey(3)
-
-
-rospy.init_node('image_converter', anonymous=True)
-image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,callback)
-rospy.spin()
+from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import MultiArrayDimension
+import random
+import numpy as np
+from math import *
+rospy.init_node("publisher")
+gripper_pub = rospy.Publisher('/gimbal_controller/command',    Float64MultiArray, queue_size=1)
+mat = Float64MultiArray()
+mat.layout.dim.append(MultiArrayDimension())
+mat.layout.dim.append(MultiArrayDimension())
+mat.data = [pi/2,pi]
+while not rospy.is_shutdown():
+    gripper_pub.publish(mat)
